d91714dbd2a3bc092f89be6714636367b2c4b9de
mahesh123-pro <237y1a04f1@mlritm.ac.in>
2026-01-14 18:55:03 +0530

    today 14jan i update my portfolio website

--- script.js
+++ script.js
@@ -110,6 +110,39 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
+// -----------------Vanilla 3D Tilt Effect for Cards------------------
+// Targeting Certification, Service, and Project cards
+const cards = document.querySelectorAll(".card, .service-card, .project-card");
+
+cards.forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10; // Max rotation deg
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)";
+  });
+});
+
+
+// -----------------Dynamic Copyright Year------------------
+const copyrightP = document.querySelector(".copyright p");
+if (copyrightP) {
+  const currentYear = new Date().getFullYear();
+  copyrightP.innerHTML = `Copyright &copy; ${currentYear} Mahesh. Made with <i class="fa-solid fa-heart" style="color: #ff004f;"></i> by Me`;
+}
+
+
 
 
 
